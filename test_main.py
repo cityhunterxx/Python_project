bcfebfc34110ea186b3ea801e9bce060b0c185d3
cityhunterxx <chatti.khalil31@gmail.com>
2022-08-29 21:37:28 +0200

    Third test pipeline

--- test_main.py
+++ test_main.py
@@ -1,8 +1,11 @@
 def sum_val(a,b):
-    return a*b
+    return a+b
 
 def diff_val(a,b):
     return a-b
+
+def multi_val(a,b):
+    return a*b
 
 def test_sum():
     assert sum_val(1,3) == 4
@@ -10,11 +13,17 @@ def test_sum():
     assert sum_val(4,4) == 8
     assert sum_val(1,2) != 5
 
-def test_dif():
+def test_diff():
     assert diff_val (2,1) == 1
     assert diff_val (4,1) == 3
     assert diff_val (6,9) == -3
     assert diff_val (2,1) != 4
+
+def test_multi():
+    assert multi_val (2,1) == 2
+    assert multi_val (4,1) == 4
+    assert multi_val (6,9) == 54
+    assert multi_val (2,1) != 3
 
 
 
