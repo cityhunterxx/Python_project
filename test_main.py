1014c83f314ddb9783144af0ea703ba3c297e8b3
cityhunterxx <chatti.khalil31@gmail.com>
2022-08-29 21:03:46 +0200

    Pytest introduction

---
+++ test_main.py
@@ -0,0 +1,21 @@
+def sum_val(a,b):
+    return a+b
+
+def diff_val(a,b):
+    return a-b
+
+def test_sum():
+    assert sum_val(1,3) == 4
+    assert sum_val(2,5) == 7
+    assert sum_val(4,4) == 8
+    assert sum_val(1,2) != 5
+
+def test_dif():
+    assert diff_val (2,1) == 1
+    assert diff_val (4,1) == 3
+    assert diff_val (6,9) == -3
+    assert diff_val (2,1) != 4
+
+
+
+
