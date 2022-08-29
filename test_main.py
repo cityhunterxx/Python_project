56523ed08994afacf42017c9dfe4abc9c5fe7487
cityhunterxx <chatti.khalil31@gmail.com>
2022-08-29 21:56:03 +0200

    Add divide function to my tests

--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+import pytest
+
 def sum_val(a,b):
     return a+b
 
@@ -6,6 +8,17 @@ def diff_val(a,b):
 
 def multi_val(a,b):
     return a*b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError('Cannot divide by Zero')
+    return a / b
+
+def test_zero_division():
+    with pytest.raises(ValueError) as err:
+        divide(1, 0)
+    assert str(err.value) == 'Cannot divide by Zero'
 
 def test_sum():
     assert sum_val(1,3) == 4
@@ -24,6 +37,10 @@ def test_multi():
     assert multi_val (4,1) == 4
     assert multi_val (6,9) == 54
     assert multi_val (2,1) != 3
+
+
+
+
 
 
 
